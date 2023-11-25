66e161b36c12f6b9782e78e0d478d14fefc44e78
ginruh <yorozuya.kenshin@gmail.com>
2023-11-26 02:39:23 +0530

    feat: added bubbletea framework

--- main.go
+++ main.go
@@ -1,34 +1,123 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	fmt.Println("Fetching Nerd Fonts latest release...")
-	release, err := fetchLatestRelease()
-	if err != nil {
-		log.Fatalln("unable to fetch latest release")
+	p := tea.NewProgram(Setup())
+	if _, err := p.Run(); err != nil {
+		log.Fatalln(err)
+		return
 	}
+}
 
-	fmt.Printf("Found release %s.\n", release.Name)
+func oldMain() {
+	fmt.Println("Fetching fonts")
+	fontAssets, err := fetchFonts()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Fetched all fonts")
+	fmt.Printf("Choose font you want to download: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+	selectedFont := scanner.Text()
 
-	// Looping through assets
-	fonts := release.Assets
-	totalFonts := 0
-	for i := 0; i < len(fonts); i++ {
-		fontName := fonts[i].Name
-		if strings.HasSuffix(fontName, ".zip") {
-			totalFonts += 1
-			fmt.Println(fontName)
+	var requiredFont *Font
+	for _, font := range fontAssets {
+		if font.Name == selectedFont {
+			requiredFont = &font
+			break
 		}
 	}
+	if requiredFont == nil {
+		log.Fatalln("user font not found")
+	}
+	fmt.Println("Beginning font download")
+	if err := requiredFont.Download(); err != nil {
+		log.Fatalln(err)
+	}
+}
 
-	fmt.Printf("\nFound %v fonts.\n", totalFonts)
+func (f *Font) Download() error {
+	downloadURL := f.DownloadURL
+	tmpDirPath := os.TempDir()
+	fmt.Println(tmpDirPath)
+	// create tmp directory for installer
+	if err := os.Mkdir(fmt.Sprintf("%s%s%s", tmpDirPath, "/", "nfinstall"), 0755); err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			return err
+		}
+	}
+	// Get request
+	res, err := http.Get(downloadURL)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	// Read all body
+	fileBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	// Save as file to tmp directory
+	if err = os.WriteFile(
+		fmt.Sprintf("%s%s%s%s%s", tmpDirPath, "/", "nfinstall", "/", f.Name),
+		fileBytes,
+		0644,
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+type Fonts []Font
+
+func (f Fonts) Get(release NFRelease) {
+	var fonts []Font
+	for _, asset := range release.Assets {
+		if strings.HasSuffix(asset.Name, ".zip") {
+			fonts = append(
+				fonts,
+				Font{Name: asset.Name, DownloadURL: asset.DownloadURL},
+			)
+
+		}
+	}
+	f = fonts
+}
+
+func fetchFonts() ([]Font, error) {
+	var fonts []Font
+	release, err := fetchLatestRelease()
+	if err != nil {
+		return fonts, err
+	}
+	for _, asset := range release.Assets {
+		if strings.HasSuffix(asset.Name, ".zip") {
+			fonts = append(
+				fonts,
+				Font{Name: asset.Name, DownloadURL: asset.DownloadURL},
+			)
+
+		}
+	}
+	return fonts, nil
 }
 
 type NFRelease struct {
